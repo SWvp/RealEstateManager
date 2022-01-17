0e4318cf2849b0e2897464eb0d4e5455876928f6
stephane warin <serkalidev@gmail.com>
2022-01-17 16:20:07 +0100

    MainActivity is configured for fragments

--- app/src/main/java/com/kardabel/realestatemanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/main/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.kardabel.realestatemanager.ui.main
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kardabel.realestatemanager.databinding.ActivityMainBinding
+import com.kardabel.realestatemanager.ui.details.DetailsFragment
+import com.kardabel.realestatemanager.ui.properties.PropertiesFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.propertiesListContainer.id, PropertiesFragment())
+                .commitNow()
+        }
 
 
-
+        if (binding.propertyDetailsContainer != null &&
+            supportFragmentManager.findFragmentById(binding.propertyDetailsContainer.id) == null
+        ) {
+            supportFragmentManager.beginTransaction()
+                .add(
+                    binding.propertyDetailsContainer.id,
+                    DetailsFragment()
+                )
+                .commitNow()
+        }
 
 
     }
