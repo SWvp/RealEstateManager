fafe6ffc398ca388a510b7a4ec60438a889eb8e2
stephane warin <serkalidev@gmail.com>
2022-01-17 14:01:46 +0100

    create Fragment for properties view

---
+++ app/src/main/java/com/kardabel/realestatemanager/ui/properties/PropertiesFragment.kt
@@ -0,0 +1,42 @@
+package com.kardabel.realestatemanager.ui.properties
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kardabel.realestatemanager.databinding.FragmentPropertiesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PropertiesFragment : Fragment() {
+
+    private var _binding: FragmentPropertiesBinding? = null
+    // TODO: Why this trouble with my little binding ?
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPropertiesBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
