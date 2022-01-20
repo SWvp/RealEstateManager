654f9102254117afc3ac06fbddc58e018a7c8e58
stephane warin <serkalidev@gmail.com>
2022-01-20 18:56:52 +0100

    let original MainActivity and rename to MainActivityOld, it will be useful to show what was wrong with the original project. Delete SecondActivity

--- app/src/main/java/com/kardabel/realestatemanager/MainActivityOld.java
+++ app/src/main/java/com/kardabel/realestatemanager/MainActivityOld.java
@@ -1,14 +1,7 @@
 package com.kardabel.realestatemanager;
 
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.kardabel.realestatemanager.R;
-import com.kardabel.realestatemanager.utils.Utils;
 
 public class MainActivityOld extends AppCompatActivity {
 
