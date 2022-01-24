82c565fd6ac990aed5837b38d02f6ac59bd75b29
stephane warin <serkalidev@gmail.com>
2022-01-24 15:12:16 +0100

    create single live event and permission view action class to start permission checker

---
+++ app/src/main/java/com/kardabel/realestatemanager/ui/main/PermissionViewAction.kt
@@ -0,0 +1,9 @@
+package com.kardabel.realestatemanager.ui.main
+
+enum class PermissionViewAction {
+
+    // SINGLE LIVE EVENT MESSAGE FOR PERMISSION
+
+    PERMISSION_ASKED,
+    PERMISSION_DENIED
+}
---
+++ app/src/main/java/com/kardabel/realestatemanager/utils/SingleLiveEvent.kt
@@ -0,0 +1,31 @@
+package com.kardabel.realestatemanager.utils
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MediatorLiveData<T>() {
+
+    private val pending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            throw IllegalStateException("Multiple observers registered but only one will be notified of changes.")
+        }
+
+        super.observe(owner) {
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        }
+    }
+
+    @MainThread
+    override fun setValue(t: T) {
+        pending.set(true)
+        super.setValue(t)
+    }
+}
