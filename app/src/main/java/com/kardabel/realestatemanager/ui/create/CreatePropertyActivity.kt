8caed2ade53c98ad4680f953ad949c2ebf53b8fc
stephane warin <serkalidev@gmail.com>
2022-01-28 16:00:37 +0100

    Create property: updtae layout with menu, recyclerview and dropdown menu || activity and viewmodel are now linked

--- app/src/main/java/com/kardabel/realestatemanager/ui/create/CreatePropertyActivity.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/create/CreatePropertyActivity.kt
@@ -1,14 +1,51 @@
 package com.kardabel.realestatemanager.ui.create
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.kardabel.realestatemanager.R
 import com.kardabel.realestatemanager.databinding.ActivityCreatePropertyBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CreatePropertyActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreatePropertyBinding
+
+    private val viewModel: CreatePropertyViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityCreatePropertyBinding.inflate(layoutInflater)
+        binding = ActivityCreatePropertyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_create, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.save_item -> {
+                saveProperty()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveProperty() {
+        viewModel.createProperty(
+            binding.inputPropertyAddress.text.toString(),
+            binding.typeDescription.text.toString(),
+            binding.type.text.toString(),
+
+        )
     }
 }
--- app/src/main/java/com/kardabel/realestatemanager/ui/create/CreatePropertyViewModel.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/create/CreatePropertyViewModel.kt
@@ -1,9 +1,42 @@
 package com.kardabel.realestatemanager.ui.create
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kardabel.realestatemanager.ApplicationDispatchers
+import com.kardabel.realestatemanager.model.PropertyEntity
+import com.kardabel.realestatemanager.repository.PropertiesRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CreatePropertyViewModel @Inject constructor(
+    private val propertiesRepository: PropertiesRepository,
+    private val applicationDispatchers: ApplicationDispatchers
+): ViewModel() {
 
 
-class CreatePropertyViewModel: ViewModel() {
+    fun createProperty(
+        address: String,
+        description: String,
+        type: String,
+        place: String,
+        price: Float
+
+        ){
+        val property = PropertyEntity(
+            address,
+            description,
+            type,
+            place,
+            price
+        )
+    }
+
+    private fun insertProperty(propertyEntity: PropertyEntity) =
+        viewModelScope.launch(applicationDispatchers.ioDispatcher) {
+            propertiesRepository.insertProperty(propertyEntity)
+        }
 
 
 }
