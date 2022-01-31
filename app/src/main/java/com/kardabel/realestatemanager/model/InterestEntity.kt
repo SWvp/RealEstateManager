dcdc4d7532fb959629516211e20873d32b969e6d
stephane warin <serkalidev@gmail.com>
2022-01-31 16:07:27 +0100

    interest -> InterestEntity.kt

---
+++ app/src/main/java/com/kardabel/realestatemanager/model/InterestEntity.kt
@@ -0,0 +1,11 @@
+package com.kardabel.realestatemanager.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "interest")
+data class InterestEntity(
+    @ColumnInfo(name = "interest") var interest: String,
+    @PrimaryKey(autoGenerate = true) val interestId: Int = 0,
+)
