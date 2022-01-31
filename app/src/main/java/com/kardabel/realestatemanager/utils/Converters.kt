42061759464dc56a0afb0ce8393813b3bc701abf
stephane warin <serkalidev@gmail.com>
2022-01-31 16:17:30 +0100

    create converters for photo

---
+++ app/src/main/java/com/kardabel/realestatemanager/utils/Converters.kt
@@ -0,0 +1,23 @@
+package com.kardabel.realestatemanager.utils
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    // Convert bitmap to byte array so we can store image in room database
+    @TypeConverter
+    fun fromBitmap(bitmap: Bitmap): ByteArray{
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    // Convert byte array to bitmap so we can retrieve photo from database
+    @TypeConverter
+    fun toBitmap(byteArray: ByteArray): Bitmap{
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+    }
+}
