ed332c7055bf28095205d0090e9fb3031fb1f516
stephane warin <serkalidev@gmail.com>
2022-02-07 15:15:33 +0100

    photoRepository code cleaning

--- app/src/main/java/com/kardabel/realestatemanager/repository/PhotoRepository.kt
+++ app/src/main/java/com/kardabel/realestatemanager/repository/PhotoRepository.kt
@@ -2,29 +2,21 @@ package com.kardabel.realestatemanager.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.kardabel.realestatemanager.model.Photo
 import com.kardabel.realestatemanager.model.PhotoEntity
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class PhotoRepository @Inject constructor() {
 
-    private var photo = mutableListOf<PhotoEntity>()
+    private var photoList = mutableListOf<Photo>()
+    private val photoToCreateLiveData = MutableLiveData<List<Photo>>()
 
-
-    // fun getPhoto(): Flow<List<PhotoEntity>> = flow{
-    //     val latestPhotoList = photo
-    //     emit(latestPhotoList)
-    // }
-
-// fun addPhoto(photoEntity: PhotoEntity){
-//     photo.add(photoEntity)
-// }
-
-    private val photoToCreateLiveData = MutableLiveData<List<PhotoEntity>>()
-
-    fun addPhoto(photoEntity: PhotoEntity) {
-        photo.add(photoEntity)
-        photoToCreateLiveData.value = photo
+    fun addPhoto(photo: Photo) {
+        photoList.add(photo)
+        photoToCreateLiveData.postValue(photoList)
     }
 
-    fun getPhotoLiveData(): LiveData<List<PhotoEntity>> = photoToCreateLiveData
+    fun getPhotoLiveData(): LiveData<List<Photo>> = photoToCreateLiveData
 }
