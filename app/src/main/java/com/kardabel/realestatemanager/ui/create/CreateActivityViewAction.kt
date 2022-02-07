9fe4fac54d21c1f43fab2ce666842b889bc30920
stephane warin <serkalidev@gmail.com>
2022-02-07 15:20:59 +0100

    create action view for single live event needed in create activity

---
+++ app/src/main/java/com/kardabel/realestatemanager/ui/create/CreateActivityViewAction.kt
@@ -0,0 +1,10 @@
+package com.kardabel.realestatemanager.ui.create
+
+
+enum class CreateActivityViewAction  {
+
+    // SINGLE LIVE EVENT MESSAGE FOR CREATE ACTIVITY
+
+    PHOTO_ERROR,
+    FINISH_ACTIVITY,
+}
