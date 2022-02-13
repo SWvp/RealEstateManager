00be99fcddc4900911a2fa6551f4c194d2ff6915
stephane warin <serkalidev@gmail.com>
2022-02-13 17:29:46 +0100

    val cant be null, say it to android

--- app/src/main/java/com/kardabel/realestatemanager/ui/properties/PropertyViewState.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/properties/PropertyViewState.kt
@@ -4,8 +4,8 @@ import android.graphics.Bitmap
 
 data class PropertyViewState(
     val propertyId: Long,
-    val type: String?,
-    val county: String?,
+    val type: String,
+    val county: String,
     val price: String?,
     val saleStatus: String,
     val vendor: String,
