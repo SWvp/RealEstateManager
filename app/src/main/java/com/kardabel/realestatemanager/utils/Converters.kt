d50cdb39d610c9cd5b1d753457c5c4662eb71ac3
stephane warin <serkalidev@gmail.com>
2022-02-13 17:06:56 +0100

    List of string converter can now return null

--- app/src/main/java/com/kardabel/realestatemanager/utils/Converters.kt
+++ app/src/main/java/com/kardabel/realestatemanager/utils/Converters.kt
@@ -9,7 +9,7 @@ class Converters {
 
     // Convert bitmap to byte array so we can store image in room database
     @TypeConverter
-    fun fromBitmap(bitmap: Bitmap): ByteArray{
+    fun fromBitmap(bitmap: Bitmap): ByteArray {
         val outputStream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
         return outputStream.toByteArray()
@@ -17,20 +17,20 @@ class Converters {
 
     // Convert byte array to bitmap so we can retrieve photo from database
     @TypeConverter
-    fun toBitmap(byteArray: ByteArray): Bitmap{
+    fun toBitmap(byteArray: ByteArray): Bitmap {
         return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     }
 
     // Convert string to list of strings
     @TypeConverter
-    fun toListOfStrings(flatStringList: String): List<String> {
-        return flatStringList.split(",")
+    fun toListOfStrings(flatStringList: String?): List<String>? {
+        return flatStringList?.split(",")
     }
 
     // Convert list of strings
     @TypeConverter
-    fun fromListOfStrings(listOfString: List<String>): String {
-        return listOfString.joinToString(",")
+    fun fromListOfStrings(listOfString: List<String>?): String? {
+        return listOfString?.joinToString(",")
     }
 
 }
