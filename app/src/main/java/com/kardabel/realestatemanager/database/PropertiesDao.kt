eb304e05d2d009ade02e0aaf2c973abb6db60917
stephane warin <serkalidev@gmail.com>
2022-02-13 15:08:18 +0100

    room dao now get his update function for photo and property

--- app/src/main/java/com/kardabel/realestatemanager/database/PropertiesDao.kt
+++ app/src/main/java/com/kardabel/realestatemanager/database/PropertiesDao.kt
@@ -3,6 +3,7 @@ package com.kardabel.realestatemanager.database
 import androidx.room.*
 import com.kardabel.realestatemanager.model.PhotoEntity
 import com.kardabel.realestatemanager.model.PropertyEntity
+import com.kardabel.realestatemanager.model.PropertyUpdate
 import com.kardabel.realestatemanager.model.PropertyWithPhoto
 import kotlinx.coroutines.flow.Flow
 
@@ -19,12 +20,18 @@ interface PropertiesDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertProperty(property: PropertyEntity): Long
 
-    @Query("DELETE FROM property WHERE user_id = :id")
-    suspend fun deletePropertyById(id: Int)
-
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertPhoto(photo: PhotoEntity)
 
     @Query("DELETE FROM photo WHERE photoId = :id")
     suspend fun deletePhotoById(id: Int)
+
+    @Update(entity = PropertyEntity::class)
+    suspend fun updateProperty(property: PropertyUpdate)
+
+    @Update
+    suspend fun updatePhoto(photo: PhotoEntity)
+
+    @Query("DELETE FROM property WHERE user_id = :id")
+    suspend fun deletePropertyById(id: Int)
 }
