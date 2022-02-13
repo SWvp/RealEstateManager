368b1d9faf0011227fdc0b7327f4f2b8ce2fad1e
stephane warin <serkalidev@gmail.com>
2022-02-13 15:09:13 +0100

    propertyUpdate can be used by Room to update specific fields from original Entity

---
+++ app/src/main/java/com/kardabel/realestatemanager/model/PropertyUpdate.kt
@@ -0,0 +1,28 @@
+package com.kardabel.realestatemanager.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+// Entity to deal with Room-update
+@Entity
+data class PropertyUpdate(
+    @ColumnInfo(name = "address") val address: String,
+    @ColumnInfo(name = "apartment_number") val apartmentNumber: String?,
+    @ColumnInfo(name = "city") val city: String,
+    @ColumnInfo(name = "zipcode") val zipcode: String,
+    @ColumnInfo(name = "county") val county: String?,
+    @ColumnInfo(name = "country") val country: String?,
+    @ColumnInfo(name = "property_description") val propertyDescription: String?,
+    @ColumnInfo(name = "type") val type: String?,
+    @ColumnInfo(name = "price") val price: Int?,
+    @ColumnInfo(name = "surface") val surface: Int?,
+    @ColumnInfo(name = "room") val room: Int?,
+    @ColumnInfo(name = "bedroom") val bedroom: Int?,
+    @ColumnInfo(name = "bathroom") val bathroom: Int?,
+    @ColumnInfo(name = "on_sale_status") val saleStatus: Boolean,
+    @ColumnInfo(name = "purchase_date") val purchaseDate: String?,
+    @ColumnInfo(name = "interest") val interest: List<String>?,
+    @PrimaryKey val propertyId: Long,
+)
