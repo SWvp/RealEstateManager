6f6137cde54ddcde3cd46623258efcee8603d475
stephane warin <serkalidev@gmail.com>
2022-02-21 18:49:51 +0100

    SearchPropertyActivity.kt: create binding manage var from viewmodel

--- app/src/main/java/com/kardabel/realestatemanager/ui/search/SearchPropertyActivity.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/search/SearchPropertyActivity.kt
@@ -1,18 +1,27 @@
 package com.kardabel.realestatemanager.ui.search
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.google.android.material.slider.RangeSlider
 import com.kardabel.realestatemanager.R
-import com.kardabel.realestatemanager.databinding.ActivityCreatePropertyBinding
 import com.kardabel.realestatemanager.databinding.ActivitySearchPropertyBinding
-import com.kardabel.realestatemanager.ui.edit.EditPropertyActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchPropertyActivity: AppCompatActivity() {
+class SearchPropertyActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchPropertyBinding
+
+    private lateinit var interestChipGroup: ChipGroup
+    // private var propertyType: String? = null
 
     private val viewModel by viewModels<SearchPropertyViewModel>()
 
@@ -21,12 +30,148 @@ class SearchPropertyActivity: AppCompatActivity() {
         binding = ActivitySearchPropertyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // var priceSliderMinValue: Int? = null
+        // var priceSliderMaxValue: Int? = null
+
+        // var surfaceSliderMinValue: Int? = null
+        // var surfaceSliderMaxValue: Int? = null
+
+        // var roomSliderMinValue: Int? = null
+        // var roomSliderMaxValue: Int? = null
+        //
+        // var numberOfPhotoSliderValue: Int? = null
+
+        viewModel.emptyInterestRepository()
+
+        interestChipGroup = binding.chipGroup
+
         // Set toolbar option
         setSupportActionBar(binding.toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        binding.toolbar.navigationIcon?.setTint(resources.getColor(R.color.white))
+        binding.toolbar.navigationIcon?.setTint(ContextCompat.getColor(this, R.color.white))
         binding.toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        // Set dropdown menu for type of property
+        val items = arrayOf(
+            getString(R.string.Flat),
+            getString(R.string.House),
+            getString(R.string.Duplex),
+            getString(R.string.Penthouse),
+            getString(R.string.Condo),
+            getString(R.string.Apartment),
+        )
+
+        val dropDownAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, R.layout.activity_create_property_type_dropdown, items
+        )
+
+        binding.propertyTypeDropdownMenu.setAdapter(dropDownAdapter)
+
+        // Manage type
+        binding.propertyTypeDropdownMenu.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                //propertyType = parent.getItemAtPosition(position).toString()
+                viewModel.propertyType(parent.getItemAtPosition(position).toString())
+            }
+
+        // Manage interest
+        binding.addInterestButton.setOnClickListener {
+            val interest = binding.inputInterest.text.toString()
+            viewModel.addInterest(interest)
+            //addNewChipInterest(interest)
+            binding.inputInterest.text?.clear()
+        }
+
+        // Price slider
+        val priceRangeSlider = binding.priceRangeSlider
+        priceRangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+            }
+
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                val values = priceRangeSlider.values
+                //priceSliderMinValue = values[0].toInt()
+                //priceSliderMaxValue = values[1].toInt()
+                viewModel.priceRange(values[0].toInt(), values[1].toInt())
+            }
+        })
+
+        // Surface slider
+        val surfaceRangeSlider = binding.surfaceRangeSlider
+        surfaceRangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+            }
+
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                val values = surfaceRangeSlider.values
+                //surfaceSliderMinValue = values[0].toInt()
+                //surfaceSliderMaxValue = values[1].toInt()
+                viewModel.surfaceRange(values[0].toInt(), values[1].toInt())
+            }
+        })
+
+        // Room slider
+        val roomRangeSlider = binding.surfaceRangeSlider
+        roomRangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+            }
+
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                val values = roomRangeSlider.values
+                //roomSliderMinValue = values[0].toInt()
+                //roomSliderMaxValue = values[1].toInt()
+                viewModel.roomRange(values[0].toInt(), values[1].toInt())
+            }
+        })
+
+        // Photo slider
+        val numberOfPhotoSlider = binding.photoSlider
+        numberOfPhotoSlider.addOnChangeListener { slider, value, fromUser ->
+            // numberOfPhotoSliderValue = value.toInt()
+            viewModel.minPhoto(value.toInt())
+        }
+
+        // Retrieve interest list from repository and display them
+        viewModel.getInterest.observe(this) { interestList ->
+            displayInterestAsChip(interestList)
+        }
+
+        binding.searchButton.setOnClickListener {
+            viewModel.search()
+        }
     }
+
+    // Interests are display as chip trough chip group
+    private fun displayInterestAsChip(interests: List<String>) {
+
+        interestChipGroup.clearCheck()
+        interestChipGroup.removeAllViewsInLayout()
+
+        val inflater = LayoutInflater.from(this)
+        for (interest in interests) {
+            val chip: Chip =
+                inflater.inflate(R.layout.item_interest_chip, interestChipGroup, false) as Chip
+            chip.text = interest
+            interestChipGroup.addView(chip)
+            chip.setOnClickListener {
+                val parent = chip.parent as ChipGroup
+                parent.removeView(chip)
+                viewModel.removeInterest(interest)
+            }
+        }
+    }
+
+    private fun addNewChipInterest(interest: String) {
+        val inflater = LayoutInflater.from(this)
+        val chip: Chip =
+            inflater.inflate(R.layout.item_interest_chip, this.interestChipGroup, false) as Chip
+        chip.text = interest
+        if (interest.length > 2) {
+            interestChipGroup.addView(chip)
+        }
+    }
+
+
 }
--- app/src/main/java/com/kardabel/realestatemanager/ui/search/SearchPropertyViewModel.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/search/SearchPropertyViewModel.kt
@@ -1,20 +1,79 @@
 package com.kardabel.realestatemanager.ui.search
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.kardabel.realestatemanager.ApplicationDispatchers
 import com.kardabel.realestatemanager.repository.CurrentSearchRepository
+import com.kardabel.realestatemanager.repository.InterestRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchPropertyViewModel @Inject constructor(
     private val searchRepository: CurrentSearchRepository,
+    private val interestRepository: InterestRepository,
     private val applicationDispatchers: ApplicationDispatchers,
 ) : ViewModel() {
 
+    var priceSliderMinValue: Int? = null
+    var priceSliderMaxValue: Int? = null
+
+    var surfaceSliderMinValue: Int? = null
+    var surfaceSliderMaxValue: Int? = null
+
+    var roomSliderMinValue: Int? = null
+    var roomSliderMaxValue: Int? = null
+
+    var numberOfPhotoSliderValue: Int? = null
+
+    private var propertyType: String? = null
+
+    val getInterest: LiveData<List<String>> = interestRepository.getInterestLiveData()
 
 
 
+    fun addInterest(interest: String) {
+        if(interest.length>2){
+            interestRepository.addInterest(interest)
+        }
+    }
 
+    fun removeInterest(interest: String) {
+
+        interestRepository.remove(interest)
+
+    }
+
+    fun propertyType(value: String) {
+        propertyType = value
+    }
+
+    fun priceRange(minValue: Int, maxValue: Int) {
+        priceSliderMinValue = minValue
+        priceSliderMaxValue = maxValue
+    }
+
+    fun surfaceRange(minValue: Int, maxValue: Int) {
+        surfaceSliderMinValue = minValue
+        surfaceSliderMaxValue = maxValue
+    }
+
+    fun roomRange(minValue: Int, maxValue: Int) {
+        roomSliderMinValue = minValue
+        roomSliderMaxValue = maxValue
+
+    }
+
+    fun minPhoto(value: Int) {
+        numberOfPhotoSliderValue = value
+    }
+
+    fun search() {
+
+    }
+
+    fun emptyInterestRepository() {
+        interestRepository.emptyInterestList()
+    }
 
 }
