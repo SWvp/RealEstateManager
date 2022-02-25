1f07ec334fd876c9cbc95e61c7325cc6f999c84d
stephane warin <serkalidev@gmail.com>
2022-02-25 14:43:02 +0100

    DetailsFragment.kt -code cleaning -fun to empty details layout if needed

--- app/src/main/java/com/kardabel/realestatemanager/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/details/DetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -22,7 +23,7 @@ class DetailsFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var photosAdapter: DetailsAdapter
-    private lateinit var interestChipGroup: ChipGroup
+
 
     private val viewModel by viewModels<DetailsViewModel>()
 
@@ -45,48 +46,76 @@ class DetailsFragment : Fragment() {
 
         recyclerView.adapter = photosAdapter
 
-        // and set the observer
+        // Observe the selected property
         viewModel.detailsLiveData.observe(this) { property ->
-            // set visibility first
-            // cause we don't want to see anything before user click on a property item
-            binding.detailDescriptionTitle.isVisible = property.visibility
-            binding.detailsMediaTitle.isVisible = property.visibility
-            binding.surfaceIcon.isVisible = property.visibility
-            binding.surfaceTitle.isVisible = property.visibility
-            binding.roomIcon.isVisible = property.visibility
-            binding.roomTitle.isVisible = property.visibility
-            binding.bedroomIcon.isVisible = property.visibility
-            binding.bedroomTitle.isVisible = property.visibility
-            binding.bathroomIcon.isVisible = property.visibility
-            binding.bathroomTitle.isVisible = property.visibility
-            binding.interestsIcon.isVisible = property.visibility
-            binding.interestsTitle.isVisible = property.visibility
-            binding.chipGroup.isVisible = property.visibility
-            binding.startSaleDate.isVisible = property.visibility
-            binding.locationIcon.isVisible = property.visibility
-            binding.locationTitle.isVisible = property.visibility
-            binding.startSaleDate.isVisible = property.visibility
-            binding.map.isVisible = property.visibility
 
-            // Set text
-            binding.descriptionText.text = property.description
-            binding.surfaceValue.text = property.surface
-            binding.roomValue.text = property.room
-            binding.bedroomValue.text = property.bedroom
-            binding.bathroomValue.text = property.bathroom
-            binding.address.text = property.address
-            binding.apartment.text = property.apartment
-            binding.city.text = property.city
-            binding.county.text = property.county
-            binding.zipCode.text = property.zipcode
-            binding.country.text = property.country
-            binding.startSaleDate.text = property.startSale
+            setVisibility(property)
+            setView(property)
 
-            Glide.with(binding.map.context).load(property.staticMap).into(binding.map)
+        }
 
-            photosAdapter.submitList(property.photos)
+        // Observe if empty the view is needed (when back from a search)
+        viewModel.isFromSearchLiveData.observe(this){
 
-            displayInterestAsChip(property.interest)
+            setViewWhenBackFromSearchActivity(it)
+
+        }
+    }
+
+    private fun setVisibility(property: DetailsViewState) {
+
+        // set visibility first
+        // cause we don't want to see anything before user click on a property item
+        binding.detailDescriptionTitle.isVisible = property.visibility
+        binding.detailsMediaTitle.isVisible = property.visibility
+        binding.surfaceIcon.isVisible = property.visibility
+        binding.surfaceTitle.isVisible = property.visibility
+        binding.roomIcon.isVisible = property.visibility
+        binding.roomTitle.isVisible = property.visibility
+        binding.bedroomIcon.isVisible = property.visibility
+        binding.bedroomTitle.isVisible = property.visibility
+        binding.bathroomIcon.isVisible = property.visibility
+        binding.bathroomTitle.isVisible = property.visibility
+        binding.interestsIcon.isVisible = property.visibility
+        binding.interestsTitle.isVisible = property.visibility
+        binding.chipGroup.isVisible = property.visibility
+        binding.startSaleDate.isVisible = property.visibility
+        binding.locationIcon.isVisible = property.visibility
+        binding.locationTitle.isVisible = property.visibility
+        binding.startSaleDate.isVisible = property.visibility
+        binding.map.isVisible = property.visibility
+
+        binding.emptyLayout?.setBackgroundColor(resources.getColor(R.color.transparent))
+    }
+
+    private fun setView(property: DetailsViewState) {
+
+        // Set text
+        binding.descriptionText.text = property.description
+        binding.surfaceValue.text = property.surface
+        binding.roomValue.text = property.room
+        binding.bedroomValue.text = property.bedroom
+        binding.bathroomValue.text = property.bathroom
+        binding.address.text = property.address
+        binding.apartment.text = property.apartment
+        binding.city.text = property.city
+        binding.county.text = property.county
+        binding.zipCode.text = property.zipcode
+        binding.country.text = property.country
+        binding.startSaleDate.text = property.startSale
+
+        Glide.with(binding.map.context).load(property.staticMap).into(binding.map)
+
+        photosAdapter.submitList(property.photos)
+
+        displayInterestAsChip(property.interest)
+    }
+
+    // Fake layer to don't show details when back from searchActivity (when search is done)
+    private fun setViewWhenBackFromSearchActivity(it: Boolean) {
+
+        if(!it){
+            binding.emptyLayout?.setBackgroundColor(resources.getColor(R.color.white))
         }
     }
 
@@ -96,7 +125,7 @@ class DetailsFragment : Fragment() {
         val inflater = LayoutInflater.from(requireContext())
 
         // Set chip group binding
-        interestChipGroup = binding.chipGroup
+        val interestChipGroup: ChipGroup = binding.chipGroup
         // Remove previous selection (for master-details)
         interestChipGroup.removeAllViews()
 
@@ -106,7 +135,7 @@ class DetailsFragment : Fragment() {
                     val chip: Chip =
                         inflater.inflate(
                             R.layout.item_interest_chip_details,
-                            this.interestChipGroup,
+                            interestChipGroup,
                             false
                         ) as Chip
                     chip.text = interest
