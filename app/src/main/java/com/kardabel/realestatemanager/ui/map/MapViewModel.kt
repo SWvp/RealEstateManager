47e924ca72c96e4495634eb64d05a89b3619bb31
stephane warin <serkalidev@gmail.com>
2022-02-28 16:57:38 +0100

    need fix to combine

--- app/src/main/java/com/kardabel/realestatemanager/ui/map/MapViewModel.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/map/MapViewModel.kt
@@ -1,20 +1,34 @@
 package com.kardabel.realestatemanager.ui.map
 
 import androidx.lifecycle.*
-import com.google.android.gms.maps.model.LatLng
 import com.kardabel.realestatemanager.ApplicationDispatchers
+import com.kardabel.realestatemanager.model.UserLocation
 import com.kardabel.realestatemanager.repository.LocationRepository
+import com.kardabel.realestatemanager.repository.PropertiesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.combine
+
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor(
     private val locationRepository: LocationRepository,
+    private val propertiesRepository: PropertiesRepository,
     private val applicationDispatchers: ApplicationDispatchers
 ) : ViewModel() {
 
-    val getMapInfo: LiveData<MapViewState> = locationRepository.fetchUpdates().asLiveData(applicationDispatchers.ioDispatcher)
+    val getMapInfo: LiveData<MapViewState> =
+        combine(locationRepository.userLocation(), propertiesRepository.getProperties()){ location, properties ->
+            location.map { userLocation ->
+                toViewState(userLocation)
+
+            }
+        }.asLiveData(applicationDispatchers.ioDispatcher)
+
+
+    private fun toViewState(userLocation: UserLocation) = MapViewState(
+
+
+    )
 
 }
