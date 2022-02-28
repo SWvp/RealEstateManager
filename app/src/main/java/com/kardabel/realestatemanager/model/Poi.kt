4a52c2739a5cc5c85737327de4f64dc3e3a0f782
stephane warin <serkalidev@gmail.com>
2022-02-28 16:56:45 +0100

    now, mapViewState return userLocation and list of poi

---
+++ app/src/main/java/com/kardabel/realestatemanager/model/Poi.kt
@@ -0,0 +1,8 @@
+package com.kardabel.realestatemanager.model
+
+import com.google.android.gms.maps.model.LatLng
+
+data class Poi(
+    val propertyId: Long,
+    val propertyLatLng: LatLng
+)
--- app/src/main/java/com/kardabel/realestatemanager/ui/map/MapViewState.kt
+++ app/src/main/java/com/kardabel/realestatemanager/ui/map/MapViewState.kt
@@ -1,8 +1,11 @@
 package com.kardabel.realestatemanager.ui.map
 
 import com.google.android.gms.maps.model.LatLng
+import com.kardabel.realestatemanager.model.Poi
+import com.kardabel.realestatemanager.model.UserLocation
 
 data class MapViewState (
-    val latLng: LatLng,
-    val zoom: Float
-        )
+    val poi: List<Poi>,
+    val userLocation: UserLocation,
+
+    )
