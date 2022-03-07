2f7aa288e831466a9affda0de48ed435cf43017a
stephane warin <serkalidev@gmail.com>
2022-03-07 10:57:04 +0100

    try to set up coroutineWorker

--- app/src/main/java/com/kardabel/realestatemanager/MainApplication.kt
+++ app/src/main/java/com/kardabel/realestatemanager/MainApplication.kt
@@ -1,10 +1,48 @@
 package com.kardabel.realestatemanager
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application(){
+class MainApplication() : Application() {
 
+
+// override fun onCreate() {
+//     super.onCreate()
+//     synchronizeDataBase()
+// }
+
+// private fun synchronizeDataBase() {
+//     CoroutineScope(Dispatchers.IO).launch {
+//         val workRequest: PeriodicWorkRequest =
+//             PeriodicWorkRequest.Builder(
+//                 SynchronizeRoomToFirestoreWorker::class.java,
+//                 1,
+//                 TimeUnit.MINUTES
+//             ).build()
+
+//         WorkManager.getInstance(applicationContext).enqueue(workRequest)
+//     }
+
+
+//     // val blurBuilder = OneTimeWorkRequestBuilder<SynchronizeRoomToFirestoreWorker>()
+//     // val workManager = WorkManager.getInstance(applicationContext)
+//     // val continuation = workManager.beginUniqueWork("start",ExistingWorkPolicy.REPLACE, blurBuilder.build())
+//     // continuation.enqueue()
+
+// }
+
+// override fun getWorkManagerConfiguration(): Configuration {
+//     TODO("Not yet implemented")
+// }
 
 }
---
+++ app/src/main/java/com/kardabel/realestatemanager/SynchronizeRoomToFirestoreWorker.kt
@@ -0,0 +1,87 @@
+package com.kardabel.realestatemanager
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.google.firebase.firestore.FirebaseFirestore
+import com.kardabel.realestatemanager.database.PropertiesDao
+import com.kardabel.realestatemanager.firestore.SendPropertyToFirestore
+import com.kardabel.realestatemanager.model.PropertyEntity
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.withContext
+
+@HiltWorker
+class SynchronizeRoomToFirestoreWorker @AssistedInject constructor(
+    context: Context,
+    params: WorkerParameters,
+    private val propertiesDao: PropertiesDao,
+    private val sendPropertyToFirestore: SendPropertyToFirestore,
+    private val applicationDispatchers: ApplicationDispatchers,
+
+    ) : CoroutineWorker(
+    context,
+    params
+
+) {
+
+    var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+
+    override suspend fun doWork(): Result {
+        withContext(applicationDispatchers.ioDispatcher) {
+
+            val roomProperties: List<PropertyEntity> = propertiesDao.getProperties()
+            val firestoreProperties = mutableListOf<PropertyEntity>()
+
+            firestore.collection("properties").addSnapshotListener { value, error ->
+
+                assert(value != null)
+                for (document in value!!.documentChanges) {
+
+                    firestoreProperties.add(document.document.toObject(PropertyEntity::class.java))
+                }
+
+                for (property in roomProperties) {
+
+                    val firestoreIdMatcher =
+                        firestoreProperties.firstOrNull { it.uid == property.uid }
+                    if (firestoreIdMatcher == null) {
+                        createFirestoreProperties(property)
+                        firestoreProperties.remove(property)
+
+                    } else if (firestoreIdMatcher.updateTimestamp < property.updateTimestamp) {
+                        updateFirestoreProperties(property)
+                        firestoreProperties.remove(property)
+                    }
+
+                }
+            }
+            propertiesDao.insertProperties(firestoreProperties)
+        }
+        return Result.success()
+
+    }
+
+    private suspend fun <T> Flow<List<T>>.flattenToList() =
+        flatMapConcat { it.asFlow() }.toList()
+
+    private fun createFirestoreProperties(property: PropertyEntity) {
+
+        sendPropertyToFirestore.createPropertyDocument(property)
+
+    }
+
+    private fun updateFirestoreProperties(property: PropertyEntity) {
+
+
+        sendPropertyToFirestore.updatePropertyDocumentFromRoom(property)
+
+    }
+
+}
