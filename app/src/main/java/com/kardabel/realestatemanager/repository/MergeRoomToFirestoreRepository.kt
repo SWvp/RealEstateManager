a55243f6a8339abee7045e0ae36009d832fb8c87
stephane warin <serkalidev@gmail.com>
2022-03-09 10:53:26 +0100

    update properties firestore documents retrieve method. Add .await from coroutines-play-services update library

--- app/src/main/java/com/kardabel/realestatemanager/repository/MergeRoomToFirestoreRepository.kt
+++ app/src/main/java/com/kardabel/realestatemanager/repository/MergeRoomToFirestoreRepository.kt
@@ -4,6 +4,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.kardabel.realestatemanager.database.PropertiesDao
 import com.kardabel.realestatemanager.firestore.SendPropertyToFirestore
 import com.kardabel.realestatemanager.model.PropertyEntity
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -20,37 +21,49 @@ class MergeRoomToFirestoreRepository @Inject constructor(
 
 
         val roomProperties: List<PropertyEntity> = propertiesDao.getProperties()
-        val firestoreProperties = mutableListOf<PropertyEntity>()
+        // val firestoreProperties = mutableListOf<PropertyEntity>()
 
-        firestore.collection("properties").addSnapshotListener { value, _ ->
+        val propertiesDocuments = firestore.collection("properties").get().await()
+        val propertiesFromFirestore = propertiesDocuments.toObjects(PropertyEntity::class.java)
 
-            assert(value != null)
+        if (roomProperties.isNotEmpty()) {
 
+            for (propertyFromRoom in roomProperties) {
 
-            for (document in value!!.documentChanges) {
-
-                firestoreProperties.add(document.document.toObject(PropertyEntity::class.java))
-            }
-
-            if (roomProperties.isNotEmpty()) {
-
-                for (property in roomProperties) {
-
-                    val firestoreIdMatcher =
-                        firestoreProperties.firstOrNull { it.uid == property.uid }
-                    if (firestoreIdMatcher == null) {
-                        createFirestoreProperties(property)
-                        firestoreProperties.remove(property)
-
-                    } else if (firestoreIdMatcher.updateTimestamp < property.updateTimestamp) {
-                        updateFirestoreProperties(property)
-                        firestoreProperties.remove(property)
+                val property =
+                    propertiesFromFirestore.firstOrNull { propertyFromFirestore ->
+                        propertyFromFirestore.uid == propertyFromRoom.uid
                     }
+                if (property == null) {
+                    createFirestoreProperties(propertyFromRoom)
+                    propertiesFromFirestore.remove(propertyFromRoom)
 
+                } else if (property.updateTimestamp < propertyFromRoom.updateTimestamp) {
+                    updateFirestoreProperties(propertyFromRoom)
+                    propertiesFromFirestore.remove(propertyFromRoom)
                 }
             }
         }
-        propertiesDao.insertProperties(firestoreProperties)
+        //  firestore.collection("properties").addSnapshotListener { value, _ ->
+        //      assert(value != null)
+        //      for (document in value!!.documentChanges) {
+        //          firestoreProperties.add(document.document.toObject(PropertyEntity::class.java))
+        //      }
+        //      if (roomProperties.isNotEmpty()) {
+        //          for (property in roomProperties) {
+        //              val firestoreIdMatcher =
+        //                  firestoreProperties.firstOrNull { it.uid == property.uid }
+        //              if (firestoreIdMatcher == null) {
+        //                  createFirestoreProperties(property)
+        //                  firestoreProperties.remove(property)
+        //              } else if (firestoreIdMatcher.updateTimestamp < property.updateTimestamp) {
+        //                  updateFirestoreProperties(property)
+        //                  firestoreProperties.remove(property)
+        //              }
+        //          }
+        //      }
+        //  }
+        propertiesDao.insertProperties(propertiesFromFirestore)
 
     }
 
