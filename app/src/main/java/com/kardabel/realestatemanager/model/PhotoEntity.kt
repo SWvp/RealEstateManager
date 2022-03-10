9f35447845308f0a13214ae92aa80c006a0cb7d5
stephane warin <serkalidev@gmail.com>
2022-03-10 09:16:59 +0100

    code cleaning

--- app/src/main/java/com/kardabel/realestatemanager/model/PhotoEntity.kt
+++ app/src/main/java/com/kardabel/realestatemanager/model/PhotoEntity.kt
@@ -10,6 +10,7 @@ data class PhotoEntity constructor(
     @ColumnInfo(name = "photo_uri") val photoUri: String,
     @ColumnInfo(name = "photo_description") var photoDescription: String,
     @ColumnInfo(name = "property_owner_id") var propertyOwnerId: Long?,
-    @ColumnInfo(name = "photo_timestamp") var photoTimestamp: Long,
+    @ColumnInfo(name = "photo_timestamp") var photoTimestamp: String,
+    @ColumnInfo(name = "photo_creation_date") var photoCreationDate: String,
     @PrimaryKey(autoGenerate = true) val photoId: Int = 0,
     )
