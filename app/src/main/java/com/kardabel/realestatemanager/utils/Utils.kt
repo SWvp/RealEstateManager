50caf5a9f797105a95eb9d72d315b39ac31150a4
stephane warin <serkalidev@gmail.com>
2022-03-12 05:38:12 +0100

    utils is now in kotlin add method to check internet connection

---
+++ app/src/main/java/com/kardabel/realestatemanager/utils/Utils.kt
@@ -0,0 +1,76 @@
+package com.kardabel.realestatemanager.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+/**
+ * Created by Philippe on 21/02/2018.
+ */
+object Utils {
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
+     * @param dollars
+     * @return
+     */
+    fun convertDollarToEuro(dollars: Int): Int {
+        return (dollars * 0.812).roundToInt()
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
+     * @return
+     */
+
+    fun todayDate(): String {
+        val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return dateFormat.format(Date())
+    }
+
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
+     * @param context
+     * @return
+     */
+    fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val network =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                network.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                network.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                network.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+        return result
+    }
+}
