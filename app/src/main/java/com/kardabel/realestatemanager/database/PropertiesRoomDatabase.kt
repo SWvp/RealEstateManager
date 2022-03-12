758851c11a5b660b0975bc472202af07e761df7b
stephane warin <serkalidev@gmail.com>
2022-03-12 05:32:24 +0100

    PropertiesRoomDatabase.kt add companion object with getdatabase for contentProvider

--- app/src/main/java/com/kardabel/realestatemanager/database/PropertiesRoomDatabase.kt
+++ app/src/main/java/com/kardabel/realestatemanager/database/PropertiesRoomDatabase.kt
@@ -1,14 +1,42 @@
 package com.kardabel.realestatemanager.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.kardabel.realestatemanager.model.PhotoEntity
 import com.kardabel.realestatemanager.model.PropertyEntity
 import com.kardabel.realestatemanager.utils.Converters
+import kotlinx.coroutines.CoroutineScope
 
 @Database(entities = [PropertyEntity::class, PhotoEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class PropertiesRoomDatabase: RoomDatabase() {
     abstract fun propertiesDao(): PropertiesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PropertiesRoomDatabase? = null
+
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): PropertiesRoomDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PropertiesRoomDatabase::class.java,
+                    "real_estate_database"
+                )
+                    .fallbackToDestructiveMigration() // Wipes and rebuilds instead of migrating if no Migration object.
+                    //.addCallback(RealEstateDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
