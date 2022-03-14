3920c2fe3412467a8a913e8e24bc6c6a56e9d2ce
stephane warin <serkalidev@gmail.com>
2022-03-14 15:27:21 +0100

    MainActivity.kt permission/navigation/property sold checker are now under only on single live event

--- app/src/main/java/com/kardabel/realestatemanager/ui/main/PermissionViewAction.kt
+++
@@ -1,9 +0,0 @@
-package com.kardabel.realestatemanager.ui.main
-
-enum class PermissionViewAction {
-
-    // SINGLE LIVE EVENT MESSAGE FOR PERMISSION
-
-    PERMISSION_ASKED,
-    PERMISSION_DENIED
-}
---
+++ app/src/main/java/com/kardabel/realestatemanager/utils/MainActivityViewAction.kt
@@ -0,0 +1,11 @@
+package com.kardabel.realestatemanager.utils
+
+enum class MainActivityViewAction {
+
+    PERMISSION_ASKED,
+    PERMISSION_DENIED,
+
+    GO_TO_EDIT_PROPERTY,
+    NO_PROPERTY_SELECTED,
+    PROPERTY_SOLD
+}
