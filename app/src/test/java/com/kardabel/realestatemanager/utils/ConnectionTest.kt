1ee41cf741b986590b034e472e29214402d43093
stephane warin <serkalidev@gmail.com>
2022-03-19 15:49:06 +0100

    ConnectionTest.kt -test ok

--- app/src/test/java/com/kardabel/realestatemanager/utils/ConnectionTest.kt
+++ app/src/test/java/com/kardabel/realestatemanager/utils/ConnectionTest.kt
@@ -19,5 +19,34 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricTestRunner::class)
 class ConnectionTest {
 
+    lateinit var connectivityManager: ConnectivityManager
 
+
+    @Before
+    fun setUp() {
+        connectivityManager = findConnectivityManager()
+    }
+
+    private fun findConnectivityManager() =
+        RuntimeEnvironment.getApplication().baseContext.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+
+    @Test
+    @Config(sdk = [Build.VERSION_CODES.M], manifest = Config.NONE, application = Application::class)
+    fun `internet connection state`() {
+        val shadowNetworkCapabilities =
+            Shadows.shadowOf(connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork))
+        // is internet
+        shadowNetworkCapabilities.addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        Assert.assertTrue(Utils.isInternetAvailable(RuntimeEnvironment.getApplication().baseContext))
+
+        // !is internet
+        shadowNetworkCapabilities.removeTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        shadowNetworkCapabilities.removeTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        shadowNetworkCapabilities.addTransportType(NetworkCapabilities.TRANSPORT_BLUETOOTH)
+        Assert.assertFalse(Utils.isInternetAvailable(RuntimeEnvironment.getApplication().baseContext))
+
+    }
 }
